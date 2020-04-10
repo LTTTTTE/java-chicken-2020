f78daee27f7cf30f74fb60ac4fa539142383efcf
juyeongjung <juyeong.dev@gmail.com>
2020-04-10 13:41:45 +0900

    feat : 각 상황별 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,25 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputPosMenuNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputFoodMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuCount() {
+        System.out.println("## 메뉴의 수량을 입력하세요");
+        return scanner.nextInt();
+    }
+
+    public static int inputPaymentNumber(int tableNumber) {
+        System.out.printf("## %d 번 테이블의 결제를 시작합니다. \n## 신용카드는 1번, 현금은 2번\n", tableNumber);
+        return scanner.nextInt();
+    }
+
 }
