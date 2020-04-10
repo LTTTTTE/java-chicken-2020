f7bb03d12b5e01c7512fa97d842d42321eeaa124
juyeongjung <juyeong.dev@gmail.com>
2020-04-10 13:33:06 +0900

    feat : table 에 메뉴를 저장할수 있도록 Map 필드 생성 및 메소드 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,10 +1,19 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
     private final int number;
+    private final Map<Menu, Integer> orders;
 
     public Table(final int number) {
         this.number = number;
+        this.orders = new HashMap<>();
+    }
+
+    public void addMenu(Menu menu, int count) {
+        orders.put(menu, count);
     }
 
     @Override
