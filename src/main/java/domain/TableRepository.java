8f7e477608d3a465c079bc68bcafeb74a037f6c1
juyeongjung <juyeong.dev@gmail.com>
2020-04-10 14:07:59 +0900

    refactor : 잘못된 테이블 번호 체크 추가

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -3,24 +3,29 @@ package domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class TableRepository {
-    private static final List<Table> tables = new ArrayList<>();
+	private static final List<Table> tables = new ArrayList<>();
 
-    static {
-        tables.add(new Table(1));
-        tables.add(new Table(2));
-        tables.add(new Table(3));
-        tables.add(new Table(5));
-        tables.add(new Table(6));
-        tables.add(new Table(8));
-    }
+	static {
+		tables.add(new Table(1));
+		tables.add(new Table(2));
+		tables.add(new Table(3));
+		tables.add(new Table(5));
+		tables.add(new Table(6));
+		tables.add(new Table(8));
+	}
 
-    public static Table findTable(int tableNumber) {
-        return tables.get(tableNumber);
-    }
+	public static Table findTable(int tableNumber) {
+		Table table = tables.get(tableNumber);
+		if (Objects.isNull(table)) {
+			throw new IllegalArgumentException("존재하지 않는 테이블 번호 입니다.");
+		}
+		return table;
+	}
 
-    public static List<Table> tables() {
-        return Collections.unmodifiableList(tables);
-    }
+	public static List<Table> tables() {
+		return Collections.unmodifiableList(tables);
+	}
 }
